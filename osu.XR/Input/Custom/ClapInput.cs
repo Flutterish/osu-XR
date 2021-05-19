4b2cc7fa411394a9ef5f7764e80b7739f30e9e03
Flutterish <CharmindYT@gmail.com>
2021-05-19 22:35:25 +0200

    Delete ClapInput.cs

--- osu.XR/Input/Custom/ClapInput.cs
+++
@@ -1,3 +0,0 @@
-				binding.IsActive.Value = ProgressBindable.Value < Math.Max( ThresholdABindable.Value, ThresholdBBindable.Value );
-			else
-				binding.IsActive.Value = ProgressBindable.Value < Math.Min( ThresholdABindable.Value, ThresholdBBindable.Value );
