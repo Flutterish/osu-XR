2c136ab652e782630ee45f07a8edb18efd5fc46b
Flutterish <CharmindYT@gmail.com>
2021-07-01 13:26:44 +0200

    added a selection animation to selection3D

--- osu.XR/Inspector/Selection3D.cs
+++ osu.XR/Inspector/Selection3D.cs
@@ -1,10 +1,14 @@
-﻿using osu.Framework.XR.Components;
+﻿using osu.Framework.Bindables;
+using osu.Framework.Graphics;
+using osu.Framework.Utils;
+using osu.Framework.XR.Components;
 using osu.Framework.XR.Graphics;
+using osu.Framework.XR.Projection;
 using osuTK;
 using osuTK.Graphics;
 
 namespace osu.XR.Inspector {
-	public class Selection3D : Model, INotInspectable {
+	public class Selection3D : Model, INotInspectable, IRenderedLast {
 		public Selection3D () {
 			Mesh = Mesh.FromOBJFile( "./Resources/selection.obj" );
 			Tint = Color4.Lime;
@@ -12,27 +16,39 @@ namespace osu.XR.Inspector {
 			AutoOffsetOrigin = Vector3.Zero;
 		}
 
+		private BindableFloat animationProgress = new( 1 );
 		Drawable3D selected;
+		Drawable3D over;
 		public Drawable3D Selected {
 			get => selected;
 			set => Select( value );
 		}
 		public void Select ( Drawable3D drawable ) {
-			Parent = drawable?.Root;
-
+			if ( selected == drawable ) return;
+			
 			selected = drawable;
-			Transform.SetParent( drawable?.Transform, transformKey );
+			if ( selected is null ) {
+				this.TransformBindableTo( animationProgress, 0, 140, Easing.Out );
+			}
+			else {
+				over = selected;
+				animationProgress.Value = 0;
+				this.TransformBindableTo( animationProgress, 1, 140, Easing.Out );
+				Parent = selected.Root;
+				Transform.SetParent( selected.Transform, transformKey );
+			}
 		}
 
 		protected override void Update () {
 			base.Update();
 
-			if ( selected is null ) {
+			if ( over is null ) {
 				return;
 			}
 
-			Scale = selected.RequiredParentSizeToFit * 1.03f;
-			Position = selected.Centre;
+			Scale = over.RequiredParentSizeToFit * 1.03f * ( 1 + ( 1 - animationProgress.Value ) * 0.3f );
+			Alpha = animationProgress.Value;
+			Position = over.Centre;
 		}
 	}
 }
