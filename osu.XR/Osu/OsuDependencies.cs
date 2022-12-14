a54dce9560d1d637a0301970ae61e9b62897dc27
Flutterish <CharmindYT@gmail.com>
2022-12-14 17:11:57 +0100

    track osu player in osuDependencies

--- osu.XR/Osu/OsuDependencies.cs
+++ osu.XR/Osu/OsuDependencies.cs
@@ -1,5 +1,15 @@
-﻿using osu.Game;
+﻿using osu.Framework.Input.Bindings;
+using osu.Framework.Input;
+using osu.Framework.Localisation;
+using osu.Framework.Screens;
+using osu.Game;
 using osu.Game.Rulesets;
+using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Objects;
+using osu.Game.Rulesets.UI;
+using osu.Game.Screens;
+using osu.Game.Screens.Play;
+using System.Reflection;
 
 namespace osu.XR.Osu;
 
@@ -8,6 +18,9 @@ public class OsuDependencies : IReadOnlyDependencyContainer {
 	public readonly Bindable<OsuGameBase?> OsuGameBase = new();
 	BindableWithCurrent<RulesetInfo?> currentRuleset = new();
 	public Bindable<RulesetInfo?> Ruleset => currentRuleset;
+
+	BindableWithCurrent<PlayerInfo?> currentPlayer = new();
+	public Bindable<PlayerInfo?> Player => currentPlayer;
 
 	enum UpdateSource {
 		None,
@@ -26,6 +39,13 @@ public class OsuDependencies : IReadOnlyDependencyContainer {
 		} );
 
 		OsuGameBase.BindValueChanged( v => {
+			Player.Value = null;
+			if ( screenStack != null ) {
+				screenStack.ScreenPushed -= onOsuScreenPushed;
+				screenStack.ScreenExited -= onOsuScreenExited;
+				screenStack = null;
+			}
+
 			if ( updateSource is UpdateSource.None ) {
 				updateSource = UpdateSource.OsuGameBase;
 				if ( v.NewValue is OsuGame osu )
@@ -44,6 +64,65 @@ public class OsuDependencies : IReadOnlyDependencyContainer {
 		} );
 	}
 
+	T GetReflected<T> ( object target, string? name = null ) {
+		var prop = target.GetType().GetProperties( BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic )
+			.Where( x => (name is null || x.Name == name) && x.CanRead && x.GetIndexParameters().Length == 0 && x.PropertyType.IsAssignableTo( typeof( T ) ) ).FirstOrDefault();
+
+		if ( prop != null )
+			return (T)prop.GetValue( target )!;
+
+		var field = target.GetType().GetFields( BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic )
+			.Where( x => (name is null || x.Name == name) && x.FieldType.IsAssignableTo( typeof( T ) ) ).FirstOrDefault();
+
+		if ( field != null )
+			return (T)field.GetValue( target )!;
+
+		return default!;
+	}
+
+	OsuScreenStack? screenStack;
+	void onOsuScreenPushed ( IScreen lastScreen, IScreen newScreen ) {
+		onOsuScreenChanged( newScreen );
+	}
+	private void onOsuScreenExited ( IScreen lastScreen, IScreen newScreen ) {
+		onOsuScreenChanged( newScreen );
+	}
+
+	void onOsuScreenChanged ( IScreen screen ) {
+		try {
+			if ( screen is not Player player ) {
+				Player.Value = null;
+				return;
+			}
+
+			var drawableRuleset = GetReflected<DrawableRuleset>( player );
+			var inputManager = GetReflected<PassThroughInputManager>( drawableRuleset );
+
+			var managerType = inputManager.GetType();
+			while ( !managerType.IsGenericType || managerType.GetGenericTypeDefinition() != typeof( RulesetInputManager<> ) ) {
+				managerType = managerType.BaseType!;
+			}
+
+			var actionType = managerType.GetGenericArguments()[0];
+			var bindings = GetReflected<KeyBindingContainer>( inputManager );
+
+			Player.Value = new PlayerInfo {
+				Player = player,
+				DrawableRuleset = drawableRuleset,
+				InputManager = inputManager,
+				RulesetActionType = actionType,
+				KeyBindingContainer = bindings,
+				Mods = GetReflected<Bindable<IReadOnlyList<Mod>>>( player ),
+				Variant = GetReflected<int>( drawableRuleset, nameof( DrawableRuleset<HitObject>.Variant ) )
+			};
+		}
+		catch ( Exception e ) {
+			ExceptionInvoked?.Invoke( @"Could not find the playfield. Please report this to the osu!xr developers", e );
+		}
+	}
+
+	public event Action<LocalisableString, Exception>? ExceptionInvoked;
+
 	private void onOsuLoaded ( OsuGameBase game ) {
 		try {
 			if ( game != OsuGameBase.Value )
@@ -51,11 +130,24 @@ public class OsuDependencies : IReadOnlyDependencyContainer {
 
 			var deps = game.Dependencies;
 			currentRuleset.Current = deps.Get<Bindable<RulesetInfo?>>();
+
+			if ( game is not OsuGame osu )
+				return;
+
+			try {
+				screenStack = GetReflected<OsuScreenStack>( osu );
+				screenStack.ScreenPushed += onOsuScreenPushed;
+				screenStack.ScreenExited += onOsuScreenExited;
+			}
+			catch ( Exception e ) {
+				ExceptionInvoked?.Invoke( @"Could not find the osu! screen stack. Please report this to the osu!xr developers", e );
+			}
 		}
 		finally {
 			OsuLoaded?.Invoke( this );
 		}
 	}
+
 	public event Action<OsuDependencies>? OsuLoaded;
 
 	public object? Get ( Type type ) {
---
+++ osu.XR/Osu/PlayerInfo.cs
@@ -0,0 +1,18 @@
+﻿using osu.Framework.Input.Bindings;
+using osu.Framework.Input;
+using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.UI;
+using osu.Game.Screens.Play;
+
+namespace osu.XR.Osu;
+
+#nullable disable
+public record PlayerInfo {
+	public Player Player { get; init; }
+	public DrawableRuleset DrawableRuleset { get; init; }
+	public PassThroughInputManager InputManager { get; init; }
+	public Type RulesetActionType { get; init; }
+	public KeyBindingContainer KeyBindingContainer { get; init; }
+	public Bindable<IReadOnlyList<Mod>> Mods { get; init; }
+	public int Variant { get; init; }
+}
