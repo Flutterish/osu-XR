faeb3821ea25c322f356b63335d76432f14cc185
Flutterish <CharmindYT@gmail.com>
2023-08-26 18:21:38 +0200

    disable touch frame stats in debug

--- osu.XR.Desktop/Program.cs
+++ osu.XR.Desktop/Program.cs
@@ -7,6 +7,7 @@ namespace osu.XR {
 	public static class Program {
 		[STAThread]
 		public static int Main () {
+			Environment.SetEnvironmentVariable( "OSU_FRAME_STATISTICS_VIA_TOUCH", "0", EnvironmentVariableTarget.Process );
 			using var host = HostXR.GetSuitableDesktopHost( "osu", new HostOptions { BindIPC = true } );
 			host.ExceptionThrown += handleException;
 			host.Run( new OsuXrGame() );
--- osu.XR.Tests/VisualTestRunner.cs
+++ osu.XR.Tests/VisualTestRunner.cs
@@ -1,7 +1,9 @@
-﻿using osu.Framework.Platform;
+﻿using osu.Framework;
+using osu.Framework.Platform;
 using osu.Framework.XR;
 using osu.XR.Tests;
 
+Environment.SetEnvironmentVariable( "OSU_FRAME_STATISTICS_VIA_TOUCH", "0", EnvironmentVariableTarget.Process );
 using ( DesktopGameHost host = HostXR.GetSuitableDesktopHost( "osu" ) ) {
 	var browser = new OsuXrTestBrowser();
 	host.Run( browser );
