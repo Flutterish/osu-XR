347dcd205f787b846a717a89bed085c9a69a8f1b
Flutterish <CharmindYT@gmail.com>
2023-09-07 23:02:50 +0200

    Update ChangelogPanel.cs

--- osu.XR/Graphics/Panels/Menu/ChangelogPanel.cs
+++ osu.XR/Graphics/Panels/Menu/ChangelogPanel.cs
@@ -29,6 +29,7 @@ public partial class ChangelogPanel : SettingsPanel {
 				#### Input
 				* Made joystick zones easier to customize.
 				* Added a windmill gesture - it allows you to use your whole arm as a joystick.
+				* Double joystick cursors now work properly.
 				#### Other
 				* Removed ""revert to default"" buttons in the {RulesetSection} section.
 				* Added notifications when ruleset binding load fails.
